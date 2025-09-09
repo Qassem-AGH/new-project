310abe521c88e602b7b52f34ae95f9049e3e8bf2
qassem <qassem.abdulghani@student.nbi-handelsakademi>
2025-09-10 00:31:33 +0200

    Updated greeting text

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello everyone this is my new-project!");
         }
     }
 }
